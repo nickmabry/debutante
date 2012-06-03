c5db783b32d2f0fc2dcf0e365ab1927cba68f097
Nick Mabry <nicholasmabry@gmail.com>
2012-06-03 10:51:07 -0500

    Updated the layout to use bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def alert_class(name)
+    case name.to_s
+    when "notice"
+      "alert alert-success"
+    when "alert"
+      "alert alert-error"
+    else
+      "alert"
+    end
+  end
 end
